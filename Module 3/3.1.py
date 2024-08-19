6babdae3dc9f10f42e9b3dbeb857642687001ec8
Yutong <yutongd@metropolia.fi>
2024-08-19 21:27:50 +0300

    added 3.1

---
+++ Module 3/3.1.py
@@ -0,0 +1,5 @@
+fish_length = input("Enter the length of the fish (cm): ")
+if int(fish_length) < 42:
+    print("Please release the fish back into the lake, the fish needs to be at least 42cm")
+else:
+    print("Congratulations, the fish is big enough")
