33615a24555e75f7423db6f69007b26240d79a1f
Yutong Deng <yutong.deng09@gmail.com>
2024-08-21 11:40:33 +0300

    Editted 5.3

--- Module 5/5.3.py
+++ Module 5/5.3.py
@@ -1,16 +1,15 @@
 number = int(input("Enter a number: "))
+check_prime = 0
 if number <= 1:
     print("The number is not prime")
 elif number <= 3:
     print("The number is prime")
 elif number >= 4:
-    for x in range(4, (number// 2) + 1):
+    for x in range(4, number - 1):
         if number % x == 0:
             print("The number is not prime")
+            check_prime = 1
             break
-        else:
-            print("The number is prime")
-            break
-else:
-    print("The number is not prime")
+    if check_prime == 0:
+        print("The number is prime")
 
