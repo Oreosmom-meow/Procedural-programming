fcb1140838916bd0a2b537ac8b39346f5c35db68
Yutong Deng <yutong.deng09@gmail.com>
2024-08-21 11:08:29 +0300

    Editted 5.3

--- Module 5/5.3.py
+++ Module 5/5.3.py
@@ -4,11 +4,13 @@ if number <= 1:
 elif number <= 3:
     print("The number is prime")
 elif number >= 4:
-    for x in range(4, number + 1):
-        if number % 6 == 1 and x != 1:
+    for x in range(4, (number// 2) + 1):
+        if number % x == 0:
             print("The number is not prime")
             break
         else:
             print("The number is prime")
             break
+else:
+    print("The number is not prime")
 
