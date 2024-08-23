65652ec924723aa430e5c5fc7d94ecfd111e66a7
Yutong Deng <yutong.deng09@gmail.com>
2024-08-23 13:51:28 +0300

    Added 8.1

---
+++ Module 8/8.1.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+def get_screen_name(number):
+    sql = f"Select ident, name, municipality from airport where ident = '{number}'"
+    #print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(f"For ICAO number: {row[0]}, the airport name is: {row[1]},the municipality is: {row[2]} .")
+    return
+
+connection = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='0917',
+         autocommit=True
+         )
+
+user_input = input("Enter a ICAO number: ")
+get_screen_name(user_input)
