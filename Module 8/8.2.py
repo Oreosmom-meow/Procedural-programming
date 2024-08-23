15c7470eb9c69e29345da36ab497dfba62c59503
Yutong Deng <yutong.deng09@gmail.com>
2024-08-23 14:12:20 +0300

    Added 8.2

---
+++ Module 8/8.2.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+def get_screen_name(code):
+    sql = f" select iso_country, type from airport where iso_country = '{code}' order by type "
+    #print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        small_occ = 0
+        medium_occ = 0
+        large_occ = 0
+        helicopter_occ = 0
+        for row in result:
+            if row[1] == 'small_airport':
+                small_occ += 1
+            elif row[1] == 'medium_airport':
+                medium_occ += 1
+            elif row[1] == 'large_airport':
+                large_occ += 1
+            elif row[1] == 'heliport':
+                helicopter_occ += 1
+        print(f"For country code = {code}, there are {small_occ} small airports, {medium_occ} medium airports, {large_occ} large airports, {helicopter_occ} helicopter airports")
+    return
+
+connection = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='****',
+         autocommit=True
+         )
+
+user_input = input("Enter a country code: ")
+get_screen_name(user_input)
