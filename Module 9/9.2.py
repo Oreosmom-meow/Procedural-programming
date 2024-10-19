f8e3f5c8fb3c9b3c0ca1713ad91376cd7cdc85b1
Yutong Deng <yutong.deng09@gmail.com>
2024-10-19 11:37:22 +0300

    Modified 9.2

--- Module 9/9.2.py
+++ Module 9/9.2.py
@@ -1,20 +1,17 @@
 class Car:
-    speed = 0
-
     def __init__(self, regi_number, max_speed, speed, trave_distance):
         self.regi_number = regi_number
         self.max_speed = max_speed
         self.speed = speed
-        Car.speed = self.speed
         self.trave_distance = trave_distance
     def accelerate(self, change_of_speed):
         self.change_of_speed = change_of_speed
-        Car.speed = self.change_of_speed + Car.speed
-        return Car.speed
+        self.speed = self.change_of_speed + self.speed
+
     def brake(self,change_of_speed):
         self.change_of_speed = change_of_speed
-        Car.speed = self.change_of_speed
-        print(f'The car has been forced to stop at the speed of {Car.speed}')
+        self.speed = self.change_of_speed
+        print(f'The car has been forced to stop at the speed of {self.speed}')
         return
 
 new_car = Car("ABC-123", "142",12, 11)
@@ -26,11 +23,11 @@ if new_speed < 0:
 while new_speed >= 0:
     print(f'---------------')
     new_car.accelerate(new_speed)
-    if int(Car.speed) >= int(new_car.max_speed):
-        print(f"The current speed is {Car.speed} km/h.")
+    if int(new_car.speed) >= int(new_car.max_speed):
+        print(f"The current speed is {new_car.speed} km/h.")
         print("The cars has exceed max speed. Emegency brake activated.")
         new_car.brake(-200)
         break
     else:
-        print(f"The current speed is {Car.speed} km/h.")
+        print(f"The current speed is {new_car.speed} km/h.")
         new_speed = int(input("Please input number to accelerate the new car: "))
