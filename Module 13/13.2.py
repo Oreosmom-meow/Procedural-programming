d68e55b8a08b2e72c08df2c69f611d43544e4f4b
Yutong Deng <yutong.deng09@gmail.com>
2024-10-20 23:07:47 +0300

    Modified 13.2! It works now

--- Module 13/13.2.py
+++ Module 13/13.2.py
@@ -8,7 +8,7 @@ connection = mysql.connector.connect(
          port= 3306,
          database='flight_game',
          user='root',
-         password='0917',
+         password='****',
          autocommit=True
          )
 
@@ -19,11 +19,14 @@ def get_screen_name(ICAO):
         cursor = connection.cursor()
         cursor.execute(sql)
         result = cursor.fetchall()
-        airport_name = result[1]
-        location = result[2]
+        airport_name = result[0][1]
+        location = result[0][2]
         response = {
             "ICAO" : ICAO,
             "Name" : airport_name,
             "Location" : location
         }
         return response
+
+if __name__ == '__main__':
+    app.run(use_reloader=True,host='127.0.0.1',port = 5000)
